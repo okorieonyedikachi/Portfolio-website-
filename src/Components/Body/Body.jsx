4f2acceaa372f010b2d51ccf293939fdfbfc9104
“FoureyedVeen” <cosmasdaniella@gmail.com>
2023-05-25 17:27:35 +0100

    Section2 : Fixed responsiveness for mobile and desktop view

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -47,33 +47,37 @@ function Body() {
                 {/* Section2 starts here */}
                 <div className='section2'>
                     <div className='section2-left'>
-                        <h2><u>About</u></h2>
-                        <h2>How it all started</h2>
-                        <p>Hi there, My name is Okorie Onyedikachi Cosmas, 
-                            a budding frontend engineer. Prior to the completion of 
-                            my bachelors degree, i found my passion for website design and 
-                            development when i walked in on a classmate working on a website for his 
-                            client he got on Fiverr, i asked him how he was able to curate these beautiful designs and put 
-                            them on the internet, He laughed and showed me how wordpress.org worked. 
-                            I had heard about website designs before then, but seeing it being done first hand was the birth of this passion
-                        </p>
-                        <div className='horizontal-line'>
-                            <hr className='hr1'></hr>
-                            <hr className='hr2'></hr>
+                        <div className='sec2-left-text'>
+                            <h1><u>About</u></h1>
+                            <h2>How it all started</h2>
+                            <p>Hi there, My name is Okorie Onyedikachi Cosmas, 
+                                a budding frontend engineer. Prior to the completion of 
+                                my bachelors degree, i found my passion for website design and 
+                                development when i walked in on a classmate working on a website for his 
+                                client he got on Fiverr, i asked him how he was able to curate these beautiful designs and put 
+                                them on the internet, He laughed and showed me how wordpress.org worked. 
+                                I had heard about website designs before then, but seeing it being done first hand was the birth of this passion
+                            </p>
+                            <div className='horizontal-line'>
+                                <hr className='hr1'></hr>
+                                <hr className='hr2'></hr>
+                            </div>
+                            <h2>How it is going</h2>
+                            <p>
+                                2+ years after delivering websites designed on Wordpress as a freelancer, 
+                                i needed more. i had learnt CSS casually during my website design journey, but 
+                                i knew i could do better. I wanted to build these websites from scratch without a CMS controlling
+                                what i had access to, so i went back to the basics
+                                to pick a better and advanced website development path. Choosing frontend 
+                                development came easy to me because of my love for CSS and
+                            </p>
                         </div>
-                        <h2>How it is going</h2>
-                        <p>
-                            2+ years after delivering websites designed on Wordpress as a freelancer, 
-                            i needed more. i had learnt CSS casually during my website design journey, but 
-                            i knew i could do better. I wanted to build these websites from scratch without a CMS controlling
-                            what i had access to, so i went back to the basics
-                            to pick a better and advanced website development path. Choosing frontend 
-                            development came easy to me because of my love for CSS and
-                        </p>
+                        
                     </div>
                     <div className='section2-right'>
-                        <div className='img-wrapper'></div>
-                        <img src={Myphoto}/>
+                        <div className='img-wrapper'>
+                            <img src={Myphoto} alt='my-photo' id='my_photo'/>
+                        </div>
                     </div>
                 </div>
                 <div className='section3'>nvbejunsiks</div>
